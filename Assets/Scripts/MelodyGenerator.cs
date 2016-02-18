4565464ea3d38b5aef1a9d08ae81a91b9069649f
Charlie Huguenard <charlie.huguenard@gmail.com>
2016-02-18 13:23:26 -0800

    comments for clarity

--- Assets/Scripts/MelodyGenerator.cs
+++ Assets/Scripts/MelodyGenerator.cs
@@ -5,6 +5,7 @@ namespace DerelictComputer
     public class MelodyGenerator : MonoBehaviour
     {
         private const int RootNote = 24; // the root if our key is C
+        private const double Latency = 0.1;
 
         public int Key;
 
@@ -12,8 +13,7 @@ namespace DerelictComputer
 
         private bool _playing;
         private int _currentTick;
-        private double _interval = 0.18; // triplet 8ths at 120BPM 
-        private double _latency = 0.1;
+        private double _interval; 
         private double _nextTickTime;
         private double _lastFootstepTime;
         private bool _isRunning;
@@ -30,24 +30,27 @@ namespace DerelictComputer
         {
             _isRunning = isRunning;
 
+            // if we had a previous footstep, start auto playing
             if (_lastFootstepTime > 0)
             {
                 // basically tap tempo with footsteps
                 var newInterval = (AudioSettings.dspTime - _lastFootstepTime)/3;
 
                 // filter out anything lower than our latency, because that probably means it's erroneous
-                if (newInterval > _latency)
+                if (newInterval > Latency)
                 {
+                    // if we're running, set the interval to 8th notes
                     if (_isRunning)
                     {
                         _interval = (AudioSettings.dspTime - _lastFootstepTime)/2;
                     }
+                    // if we're walking, set the interval to triplet 8th notes
                     else
                     {
                         _interval = (AudioSettings.dspTime - _lastFootstepTime)/3;
                     }
-                    Debug.Log(_interval);
 
+                    // if we weren't auto-playing, start doing that
                     if (!_playing)
                     {
                         _nextTickTime = AudioSettings.dspTime;
@@ -55,6 +58,7 @@ namespace DerelictComputer
                     }
                 }
             }
+            // if we just reset, just play a note now
             else
             {
                 _tuba.PlayNote(SelectNote(), _lastFootstepTime, SelectDuration());
@@ -75,13 +79,17 @@ namespace DerelictComputer
                 return;
             }
 
-            if (AudioSettings.dspTime + _latency > _nextTickTime)
+            if (AudioSettings.dspTime + Latency > _nextTickTime)
             {
                 if (SelectShouldPlayNote())
                 {
                     _tuba.PlayNote(SelectNote(), _nextTickTime, SelectDuration());
                 }
                 _nextTickTime += _interval;
+                
+                // increment the tick
+                // if we're running, wrap on 8 ticks (4/4, 8th note tick)
+                // if we're walking, wrap on 6 ticks (6/8, 8th note tick)
                 _currentTick = (_currentTick + 1)%(_isRunning ? 8 : 6);
             }
         }
@@ -90,35 +98,42 @@ namespace DerelictComputer
         {
             if (_isRunning)
             {
+                // always play on 1 and 3
                 if (_currentTick == 0 || _currentTick == 4)
                 {
                     return true;
                 }
 
+                // usually play on 2 and 4
                 if (_currentTick == 2 || _currentTick == 6)
                 {
                     return Random.value < 0.85f;
                 }
 
+                // occasionally play the upbeats
                 return Random.value < 0.25f;
             }
             else
             {
+                // always play on the downbeats
                 if (_currentTick == 0 || _currentTick == 3)
                 {
                     return true;
                 }
 
+                // usually play the pickup to beat 2
                 if (_currentTick == 2)
                 {
                     return Random.value < 0.75f;
                 }
 
+                // sometimes play the pickup to beat 1
                 if (_currentTick == 5)
                 {
                     return Random.value < 0.5f;
                 }
 
+                // and occasionally play the others
                 return Random.value < 0.1f;
             }
         }
@@ -127,50 +142,60 @@ namespace DerelictComputer
         {
             if (_isRunning)
             {
+                // play the root on 1 and 3
                 if (_currentTick == 0 || _currentTick == 4)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 12 : (Random.value < 0.75 ? 0 : 24));
                 }
 
+                // play the 5th on 2 and 4
                 if (_currentTick == 2 || _currentTick == 6)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 7 : 19);
                 }
 
+                // play either the 6th or 3rd on the upbeats of 1 and 3
                 if (_currentTick == 1 || _currentTick == 5)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 9 : 16);
                 }
 
+                // play either the 7th or 2nd on the upbeats of 2 and 4
                 return Key + RootNote + (Random.value < 0.5 ? 11 : 14);
             }
             else
             {
+                // play the root on 1
                 if (_currentTick == 0)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 12 : (Random.value < 0.75 ? 0 : 24));
                 }
 
+                // play the 5th or 4th on 2
                 if (_currentTick == 3)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 7 : 17);
                 }
 
+                // play the 3rd on the pickup to 2
                 if (_currentTick == 2)
                 {
                     return Key + RootNote + 16;
                 }
 
+                // play the 6th or 3rd on the and of 2
                 if (_currentTick == 4)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 9 : 16);
                 }
 
+                // play the 7th or 2nd on the pickup to 1
                 if (_currentTick == 5)
                 {
                     return Key + RootNote + (Random.value < 0.5 ? 11 : 14);
                 }
 
+                // or play the 7th or 2nd on the other notes
                 return Key + RootNote + (Random.value < 0.5 ? 11 : 14);
             }
         }
